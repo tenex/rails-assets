3ce2c1948b97fbf8fadfa14af3c1e0c2230f7ea4
Tymon Tobolski <i@teamon.eu>
2013-08-01 02:10:51 +0200

    Bundler setup

--- lib/rails-assets.rb
+++ lib/rails-assets.rb
@@ -1,5 +1,8 @@
 $:.unshift(File.expand_path("../", __FILE__))
 
+require "bundler/setup"
+Bundler.setup
+
 require "rails-assets/config"
 require "rails-assets/component"
 require "rails-assets/utils"
