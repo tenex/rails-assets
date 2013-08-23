0036646c392fb10d181cc50be7698397e0bca133
Tymon Tobolski <i@teamon.eu>
2013-08-23 15:43:52 +0200

    Changed priority of manifest files

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,6 +2,6 @@ GEM_PREFIX = "rails-assets-"
 BOWER_BIN = "bower"
 GIT_BIN = "git"
 RAKE_BIN = "rake"
-BOWER_MANIFESTS = ["bower.json", "package.json", "component.json"]
+BOWER_MANIFESTS = ["component.json", "package.json", "bower.json"]
 REDIS_NAMESPACE = "rails-assets"
 DATA_DIR = ENV["DATA_DIR"] || File.expand_path("../../../data", __FILE__)
