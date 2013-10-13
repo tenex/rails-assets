bb0f84380c6812ea035add8e3df15dde29111c38
Adam Stankiewicz <sheerun@sher.pl>
2013-10-13 19:46:22 +0200

    fix: Move Path class to separate file (autoloading issues)

--- app/models/build/paths.rb
+++ app/models/build/paths.rb
@@ -60,48 +60,4 @@ module Build
       end.compact.first
     end
   end
-
-  class Path < Pathname
-
-    # Extensioins are sorted by priority
-    def self.extension_classes
-      {
-        javascripts: ['coffee', 'js'],
-        stylesheets: ['sass', 'scss', 'less', 'css'],
-        images: ['png', 'jpg', 'jpeg', 'gif']
-      }
-    end
-
-    def initialize(path = nil)
-      super(path || '.')
-    end
-
-    def minified?
-      to_s.include?('.min.')
-    end
-
-    def member_of?(klass)
-      extension?(Path.extension_classes.fetch(klass, []))
-    end
-
-    def descendant?(directory)
-      !relative_path_from(Path.new(directory)).to_s.split('/').include?('..')
-    end
-
-    def prefix(path)
-      Path.new(path).join(self)
-    end
-
-    def join(*elements)
-      Path.new(super(*elements))
-    end
-
-    def extension?(extensions)
-      extensions.any? do |extension|
-        !!to_s.match(/\.#{extension}(?:[\W]|$)/)
-      end
-    end
-
-  end
-
 end
