babd26409de53f955a1e46ba4a1efd0ed70af57b
Adam Stankiewicz <sheerun@sher.pl>
2013-10-16 16:25:27 +0200

    Better error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+
+  rescue_from Exception, :with => :show_error
+
+  def show_error(e)
+    raise if !request.xhr? || !Rails.env.development?
+
+    render :json => { :message => e.message, :log => e.backtrace },
+      :status => :unprocessable_entity
+  end
 end
---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,46 @@
+class ErrorsController < ActionController::Base
+
+  # Because we don't inherit from ApplicationController
+  layout 'error'
+
+  # You can use these method in views
+  helper_method :status_code, :status_name, :error_message
+
+  # You can edit format.json for proper API response format.
+  def show
+    custom_template = template_exists?(status_code, 'errors')
+
+    respond_to do |format|
+      format.html { render action: custom_template ? status_code.to_s : 'show' }
+      format.json { render json: { status: status_code, message: error_message, log: error_trace }, status: status_code }
+    end
+  end
+
+  protected
+
+  def status_code
+    (request.path.match(/\d{3}/) || ['500'])[0].to_i
+  end
+
+  def status_name
+    Rack::Utils::HTTP_STATUS_CODES.fetch(status_code, "Internal Server Error")
+  end
+
+  def error_message
+    if status_code != 500 && exception
+      exception.message
+    else
+      "Our administrator has been notified about this event."
+    end
+  end
+
+  def error_trace
+    if status_code != 500 && exception
+      exception.backtrace
+    end
+  end
+
+  def exception
+    env['action_dispatch.exception']
+  end
+end
