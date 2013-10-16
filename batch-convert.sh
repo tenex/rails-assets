5f8f80c978ba39ccf27472bf69ef3e124c3df763
Adam Stankiewicz <sheerun@sher.pl>
2013-10-16 17:46:38 +0200

    Update batch-convert script

--- batch-convert.sh
+++ batch-convert.sh
@@ -1,16 +1,6 @@
-mkdir -p "out"
+HOST="localhost:3000"
 
-echo "Searching $1"
-
-
-for pkg in $(bower search $1 | awk '{print $1}' | tail +3); do
-  pkg=$(echo $pkg | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
-  printf $pkg
-
-  code=$(curl -XPOST http://localhost:9292/convert.json -d name=$pkg -w "%{http_code}" -o out/$pkg.json 2> /dev/null)
-  if [ $code = "200" ]; then
-    echo " - OK"
-  else
-    echo " - ERROR"
-  fi;
+for pkg in angular-bootstrap angular-cookies moment-range angular-date-range-picker angular-faye angular-mocks angular-mousewheel hamsterjs angular-resource angular-scenario angular-tagger angular-ui-router angular-ui-select2 select2 angular-ujs angular-unstable bootstrap-datepicker css-modal griddle handlebars jquery.cookie responsive-nav opentip purl resizeend swipe angular-route normalize-css validate-patched classie angular angular-digest-interceptor angular-bootstrap-datetimepicker bootstrap-datetimepicker sugar lodash angular-prevent-default elusive-iconfont underscore jquery bootstrap fastclick jquery-mousewheel leaflet spin.js uglify-js microplugin moment d3 selectize sifter momentjs angular-dragdrop jquery-ui mathjs knockout-semantic sass-bootstrap-glyphicons; do
+  echo $pkg
+  curl -v http://$HOST/components.json -d "component[name]=$pkg"
 done
