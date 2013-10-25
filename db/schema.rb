2c2dcc8de9d73f4b870df5cb35f8533f4c0ae499
Adam Stankiewicz <sheerun@sher.pl>
2013-10-25 17:31:29 +0200

    Update schema.db after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131016123853) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-  enable_extension "hstore"
-
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
