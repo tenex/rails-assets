79f692338d9f9641625a7e282a730217d152d970
Adam Stankiewicz <sheerun@sher.pl>
2013-11-05 15:23:03 +0100

    Move locks to separate directory

--- app/models/build/file_store.rb
+++ app/models/build/file_store.rb
@@ -2,7 +2,13 @@ module Build
   module FileStore extend self
 
     def with_lock(lock, &block)
-      File.open(File.join(DATA_DIR, "#{lock.to_s}.lock"), "w") do |f|
+      locks_dir = File.join(DATA_DIR, 'locks').to_s
+
+      unless Dir.exist?(locks_dir)
+        FileUtils.mkdir_p(File.join(DATA_DIR, 'locks').to_s)
+      end
+
+      File.open(File.join(DATA_DIR, "locks/#{lock.to_s}.lock"), "w") do |f|
         f.flock(File::LOCK_EX)
         f.write Process.pid
         result = block.call(f)
