fa96cd3df998add7dc8ea251c17a8541b43c18b2
Adam Stankiewicz <sheerun@sher.pl>
2013-11-10 01:04:59 +0100

    fix: Ensure lock file is unlocked

--- app/models/build/file_store.rb
+++ app/models/build/file_store.rb
@@ -8,12 +8,13 @@ module Build
         FileUtils.mkdir_p(File.join(DATA_DIR, 'locks').to_s)
       end
 
-      File.open(File.join(DATA_DIR, "locks/#{lock.to_s}.lock"), "w") do |f|
+      File.open(File.join(DATA_DIR, "locks/#{lock.to_s}.lock"), "w+") do |f|
         f.flock(File::LOCK_EX)
-        f.write Process.pid
-        result = block.call(f)
-        f.flock(File::LOCK_UN)
-        result
+        begin
+          yield
+        ensure
+          f.flock(File::LOCK_UN)
+        end
       end
     end
 
