0c1ea308d02e4d6c38ebf9838fd90513e57767aa
Adam Stankiewicz <sheerun@sher.pl>
2013-12-15 03:04:39 +0100

    Always eagerly load application (auotload errors)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ RailsAssets::Application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
