ef9aca32b3dc5c697bd2e4d391865fd194103fa5
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2013-12-20 16:04:59 +0100

    Break building gems on demand to separate locks
    This way when two requests want to build the same gem they won't build
    the same gem twice.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,19 +8,17 @@ class MainController < ApplicationController
   end
 
   def dependencies
-    lock_name = "request-#{Digest::MD5.hexdigest(params[:gems].to_s)}.lock"
+    gem_names = params[:gems].to_s
+      .split(",")
+      .select {|e| e.start_with?(GEM_PREFIX) }
+      .map { |e| e.gsub(GEM_PREFIX, "") }
 
-    Build::Locking.with_lock(lock_name) do
-      gem_names = params[:gems].to_s
-        .split(",")
-        .select {|e| e.start_with?(GEM_PREFIX) }
-        .map { |e| e.gsub(GEM_PREFIX, "") }
+    pool = Thread.pool(5)
 
-      pool = Thread.pool(5)
-
-      gem_names.each do |name|
-        if Component.needs_build?(name)
-          pool.process do
+    gem_names.each do |name|
+      pool.process do
+        Build::Locking.with_lock("build-in-dependencies-#{name}") do
+          if Component.needs_build?(name)
             begin
               Build::Converter.run!(name, "latest")
             rescue Exception => e
@@ -31,32 +29,32 @@ class MainController < ApplicationController
           end
         end
       end
-
-      pool.shutdown
-
-      gems = gem_names.flat_map do |name|
-
-        component = Component.find_by(name: name)
-
-        if component # && component.built?
-          component.versions.built.map do |v|
-            {
-              name:         "#{GEM_PREFIX}#{name}",
-              platform:     "ruby",
-              number:       v.string,
-              dependencies: v.dependencies || {}
-            }
-          end
-        else
-          []
-        end
-      end
-
-      Rails.logger.info(params)
-      Rails.logger.info(gems)
-
-      params[:json] ? render(json: gems) : render(text: Marshal.dump(gems))
     end
+
+    pool.shutdown
+
+    gems = gem_names.flat_map do |name|
+
+      component = Component.find_by(name: name)
+
+      if component # && component.built?
+        component.versions.built.map do |v|
+          {
+            name:         "#{GEM_PREFIX}#{name}",
+            platform:     "ruby",
+            number:       v.string,
+            dependencies: v.dependencies || {}
+          }
+        end
+      else
+        []
+      end
+    end
+
+    Rails.logger.info(params)
+    Rails.logger.info(gems)
+
+    params[:json] ? render(json: gems) : render(text: Marshal.dump(gems))
   end
 
   private
