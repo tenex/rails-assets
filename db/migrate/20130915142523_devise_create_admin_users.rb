8399e75df780b5caa573de0cea23b5466ec4c80e
Adam Stankiewicz <sheerun@sher.pl>
2013-12-25 19:45:39 +0100

    fix: Do not use model in migration

--- db/migrate/20130915142523_devise_create_admin_users.rb
+++ db/migrate/20130915142523_devise_create_admin_users.rb
@@ -2,7 +2,6 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
   end
 
   def change
