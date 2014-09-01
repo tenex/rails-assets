3b818084c61ca591a26197c44e85a9f7bb0824ee
Adam Stankiewicz <sheerun@sher.pl>
2014-09-01 17:46:29 +0200

    Revert "Use Rails format routing to match the RubyGems API"
    This reverts commit 566ed9488b68c2c0bddf2b6c8386acf7ff540569.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,42 +17,40 @@ class MainController < ApplicationController
 
   def dependencies
     if params[:gems].blank?
-      gems = []
-    else
-      gem_names = params[:gems].to_s
-        .split(",")
-        .select {|e| e.start_with?(GEM_PREFIX) }
-        .map { |e| e.gsub(GEM_PREFIX, "") }
-
-      gem_names.each do |name|
-        if Component.needs_build?(name)
-          ::UpdateComponent.perform_async(name)
-        end
-      end
-
-      if Version.pending_index.count > 0
-        Reindex.new.perform
-      end
-
-      gems = Component.where(name: gem_names).to_a.flat_map do |component|
-        component.versions.builded.map do |v|
-          {
-            name:         "#{GEM_PREFIX}#{component.name}",
-            platform:     "ruby",
-            number:       v.string,
-            dependencies: v.dependencies || {}
-          }
-        end
-      end
-
-      Rails.logger.info(params)
-      Rails.logger.info(gems)
+      params[:json] ? render(json: []) : render(text: Marshal.dump([]))
+      return
     end
 
-    respond_to do |format|
-      format.json { render json: gems }
-      format.all { render text: Marshal.dump(gems) }
+    gem_names = params[:gems].to_s
+      .split(",")
+      .select {|e| e.start_with?(GEM_PREFIX) }
+      .map { |e| e.gsub(GEM_PREFIX, "") }
+
+    gem_names.each do |name|
+      if Component.needs_build?(name)
+        ::UpdateComponent.perform_async(name)
+      end
     end
+
+    if Version.pending_index.count > 0
+      Reindex.new.perform
+    end
+
+    gems = Component.where(name: gem_names).to_a.flat_map do |component|
+      component.versions.builded.map do |v|
+        {
+          name:         "#{GEM_PREFIX}#{component.name}",
+          platform:     "ruby",
+          number:       v.string,
+          dependencies: v.dependencies || {}
+        }
+      end
+    end
+
+    Rails.logger.info(params)
+    Rails.logger.info(gems)
+
+    params[:json] ? render(json: gems) : render(text: Marshal.dump(gems))
   end
 
   def packages
