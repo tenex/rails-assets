b7eae2f3adcd141dcc2aabc30ffe89f3d10d7f5f
Adam Stankiewicz <sheerun@sher.pl>
2014-10-02 21:03:49 +0200

    Allow for on-the-fly build fail, fixes #176

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -28,7 +28,7 @@ class MainController < ApplicationController
 
     gem_names.each do |name|
       if Component.needs_build?(name)
-        ::BuildVersion.new.perform(name, 'latest')
+        ::BuildVersion.new.perform(name, 'latest') rescue nil
         ::UpdateComponent.perform_async(name)
       end
     end
