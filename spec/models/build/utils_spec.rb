ec4514a7eec230b7ff54f112a72a9e1de3a5a657
Daniel <daniel.montenegro@monterail.com>
2015-11-30 14:56:58 +0100

    Change of the Util Module's tests
                    Change of 8 tests expected result. This change is related to version number parsing made by the fix_version_string function.
                    The changes were done since the new expected behavior will be that when the version given by the user is of the form '~1.x.x'
                    instead of returning '~> 1.0.0' it returns  '~> 1'

--- spec/models/build/utils_spec.rb
+++ spec/models/build/utils_spec.rb
@@ -44,11 +44,11 @@ module Build
       end
 
       specify do
-        expect(Utils.fix_version_string('2.3.x')).to eq('~> 2.3.0')
+        expect(Utils.fix_version_string('2.3.x')).to eq('~> 2.3')
       end
 
       specify do
-        expect(Utils.fix_version_string('v2.3.x')).to eq('~> 2.3.0')
+        expect(Utils.fix_version_string('v2.3.x')).to eq('~> 2.3')
       end
 
       specify do
@@ -72,19 +72,19 @@ module Build
       end
 
       specify do
-        expect(Utils.fix_version_string('~1.x')).to eq('~> 1.0')
+        expect(Utils.fix_version_string('~1.x')).to eq('~> 1')
       end
 
       specify do
-        expect(Utils.fix_version_string('1.x')).to eq('~> 1.0')
+        expect(Utils.fix_version_string('1.x')).to eq('~> 1')
       end
 
       specify do
-        expect(Utils.fix_version_string('~1.*')).to eq('~> 1.0')
+        expect(Utils.fix_version_string('~1.*')).to eq('~> 1')
       end
 
       specify do
-        expect(Utils.fix_version_string('1.*')).to eq('~> 1.0')
+        expect(Utils.fix_version_string('1.*')).to eq('~> 1')
       end
 
       specify do
@@ -153,7 +153,7 @@ module Build
 
       specify do
         expect(Utils.fix_version_string('~v1.0.x')).
-          to eq("~> 1.0.0")
+          to eq("~> 1.0")
       end
 
       specify do
@@ -179,7 +179,7 @@ module Build
 
       specify do
         expect(Utils.fix_version_string('^1.2.x')).
-          to eq(">= 1.2.0, < 2")
+          to eq(">= 1.2, < 2")
       end
 
       specify do
