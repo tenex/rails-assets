1c6fe89c3c561965157f4264cef44dd97ba540dd
Liam Bowen <LiamBowen@gmail.com>
2016-01-20 01:35:56 -0500

    Don't require SSL in development

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -116,6 +116,6 @@ class MainController < ApplicationController
   private
 
   def redirect_to_https
-    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+    redirect_to protocol: 'https://' unless request.ssl? || request.local? || Rails.env.development?
   end
 end
