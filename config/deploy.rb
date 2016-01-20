efc6978f567c3ae5872bdfdfcfa995c481387932
Liam Bowen <LiamBowen@gmail.com>
2016-01-20 04:06:28 -0500

    Add rough draft of config/deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'rails-assets'
+set :repo_url, 'git@github.com:tenex/rails-assets.git'
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/rails-assets/rails-apps/rails-assets'
+set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+set :pty, false
+set(:linked_files,
+    fetch(:linked_files, []).push(
+      'config/database.yml', 'config/application.yml'))
+# fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml'))
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :keep_releases, 5
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
