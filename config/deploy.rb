3f0da95104aff488f7e244ba168183619da47c31
Liam Bowen <LiamBowen@gmail.com>
2016-01-20 09:23:50 -0500

    Get rid of useless comments, code review fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,11 @@ lock '3.4.0'
 
 set :application, 'rails-assets'
 set :repo_url, 'git@github.com:tenex/rails-assets.git'
-
-# Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/rails-assets/rails-apps/rails-assets'
+set :deploy_user, -> { fetch(:application) }
+set :deploy_to, lambda {
+  "/home/#{fetch :deploy_user}/rails-apps/#{fetch :application}"
+}
 set :scm, :git
 set :format, :pretty
 set :log_level, :debug
@@ -16,11 +15,7 @@ set :pty, false
 set(:linked_files,
     fetch(:linked_files, []).push(
       'config/database.yml', 'config/application.yml'))
-# fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml'))
-# Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 set :keep_releases, 5
