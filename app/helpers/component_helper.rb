9557ad6c66b36a4f45efe900fcbd0568e81b2c15
Liam Bowen <LiamBowen@gmail.com>
2016-03-06 20:29:56 -0500

    module_function ftw

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,21 +1,27 @@
-module ComponentHelper extend self
-                       def generate_components_json
-                         ids = Version.indexed.select(:component_id)
-                                      .to_a.map(&:component_id)
+# Component Helper
+module ComponentHelper
+  module_function
 
-                         Component.includes(:versions).references(:versions)
-                                  .where(id: ids)
-                                  .where("versions.build_status = 'indexed'")
-                                  .select('components.*, versions.string')
-                                  .to_a.map { |c| component_data(c) }
-                       end
+  def generate_components_json
+    ids = Version
+          .indexed
+          .select(:component_id)
+          .to_a.map(&:component_id)
+    Component
+      .includes(:versions)
+      .references(:versions)
+      .where(id: ids)
+      .where("versions.build_status = 'indexed'")
+      .select('components.*, versions.string')
+      .to_a.map { |c| component_data(c) }
+  end
 
-                       def component_data(component)
-                         {
-                           name:         component.name,
-                           description:  component.description,
-                           homepage:     component.homepage,
-                           versions:     component.versions.map(&:string)
-                         }
-                       end
+  def component_data(component)
+    {
+      name:         component.name,
+      description:  component.description,
+      homepage:     component.homepage,
+      versions:     component.versions.map(&:string)
+    }
+  end
 end
