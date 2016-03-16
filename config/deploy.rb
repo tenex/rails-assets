6496ae1a000938b3fefb8352537a64e0a95c815f
Liam Bowen <LiamBowen@gmail.com>
2016-03-16 15:18:09 -0400

    get rid of template junk

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,14 +69,3 @@ namespace :foreman do
     end
   end
 end
-
-namespace :deploy do
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-end
