786511334532c8fe87ec21624235fc0a505c7c83
Liam Bowen <LiamBowen@gmail.com>
2016-03-19 22:38:25 -0400

    fix unknown instance problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,8 @@ set :npm_env_variables, {}
 
 task :restart_workers do
   on roles(:worker) do
-    sudo 'restart sidekiq'
+    sudo 'stop workers || true'
+    sudo 'start workers'
   end
 end
-after "deploy:published", 'restart_workers'
+after 'deploy:published', 'restart_workers'
