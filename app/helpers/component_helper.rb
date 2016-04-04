8530065f2d55c22b2395b62c40240b02fa8c6a42
Liam Bowen <LiamBowen@gmail.com>
2016-04-04 23:35:50 +0000

    adapt generate_components_json to use cache

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -3,14 +3,10 @@ module ComponentHelper
   extend self
 
   def generate_components_json
-    ids = Version.indexed.pluck(:component_id)
     Component
-      .includes(:versions)
-      .references(:versions)
-      .where(id: ids)
-      .where("versions.build_status = 'indexed'")
-      .select('components.*, versions.string')
-      .to_a.map { |c| component_data(c) }
+      .joins(:versions)
+      .where(versions: { build_status: 'indexed' })
+      .pluck(:summary_cache)
   end
 
   def component_data(component)
