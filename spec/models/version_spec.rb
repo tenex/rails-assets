1480775bab1251fba4bf9a934169e8bbde17416b
Liam Bowen <LiamBowen@gmail.com>
2016-04-04 20:13:46 +0000

    Update model annotations

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: versions
+#
+#  id            :integer          not null, primary key
+#  component_id  :integer
+#  string        :string(255)
+#  dependencies  :hstore
+#  created_at    :datetime
+#  updated_at    :datetime
+#  build_status  :string(255)
+#  build_message :text
+#  asset_paths   :text             default([]), is an Array
+#  main_paths    :text             default([]), is an Array
+#  rebuild       :boolean          default(FALSE)
+#  bower_version :string(255)
+#  position      :string(1023)
+#  prerelease    :boolean          default(FALSE)
+#
+
 require 'spec_helper'
 
 describe Version do
