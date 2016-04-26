b658269aa87fad75c10354b5effccb36bf8c042a
Liam Bowen <LiamBowen@gmail.com>
2016-04-26 06:00:22 -0400

    require these keys via ! notation

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY']
+  publishable_key: Figaro.env.stripe_publishable_key!,
+  secret_key:      Figaro.env.stripe_secret_key!
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
