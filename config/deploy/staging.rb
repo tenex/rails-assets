89af4ac15445af4cc62350f69a878994be76734b
Liam Bowen <LiamBowen@gmail.com>
2016-04-26 05:13:03 -0400

    change staging name so it exists

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 server(
-  'staging.rails-assets.tenex.tech',
+  'staging.rails-assets.org',
   user: fetch(:deploy_user),
   roles: %w(app db web worker)
 )
