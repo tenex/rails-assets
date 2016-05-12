4d740e55b45176ab2c2699adbc39a5acb9522870
Liam Bowen <LiamBowen@gmail.com>
2016-05-12 19:52:58 -0400

    just link everything in public

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,21 +15,12 @@ set :log_level, :debug
 set :pty, false
 set(:linked_files,
     fetch(:linked_files, []).push(
-      'config/application.yml',
-      'public/components.json',
-      'public/prerelease_specs.4.8',
-      'public/prerelease_specs.4.8.gz',
-      'public/specs.4.8',
-      'public/specs.4.8.gz',
-      'public/latest_specs.4.8',
-      'public/latest_specs.4.8.gz',
-      'public/packages.json'
+      'config/application.yml'
     ))
 
 set(:linked_dirs,
     fetch(:linked_dirs, []).push(
-      'public/gems',
-      'public/quick',
+      'public',
       'tmp/cache',
       'log'
     ))
