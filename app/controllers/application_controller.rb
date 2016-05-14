c7a9bb5ba2ec771b6b90f341c91ce1c4dae66ffb
Liam Bowen <LiamBowen@gmail.com>
2016-05-13 22:58:20 -0400

    change error handling in ApplicationController
    get rid of bizarre error handling, and replace it with better stuff.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,7 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception, with: :show_error
+  rescue_from ActionController::UnknownFormat, with: :handle_unknown_format
 
-  def show_error(e)
-    Rails.logger.error(e)
-
-    raise if !request.xhr? || !Rails.env.development?
-
-    if Rails.env.development?
-      render json: { message: e.message, log: e.backtrace.join("\n") },
-             status: :unprocessable_entity
-    else
-      render json: { message: e.message }, status: :unprocessable_entity
-    end
+  def handle_unknown_format
+    render nothing: true, status: 406
   end
 end
