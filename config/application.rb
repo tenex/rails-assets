38040992d0175c34cb9191907128adaaed8fe992
Liam Bowen <LiamBowen@gmail.com>
2016-05-15 16:00:52 -0400

    rubocop/style cleanup

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(:default, Rails.env)
 
 require_relative '../app/workers/update_scheduler'
 
 module RailsAssetsApp
+  # Rails Assets Application
   class Application < Rails::Application
-    config.cache_store = :redis_store, ENV['REDIS_URL']
-    Rails.configuration.x.hostname = 'rails-assets.org'
-    Rails.configuration.x.inline_ng_templates = true
+    config.cache_store = :redis_store, Figaro.env.redis_url
+    config.x.hostname = 'rails-assets.org'
+    config.x.inline_ng_templates = true
   end
 end
