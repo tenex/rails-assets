df91a948d1464d272a9421ebfaa8be08d74a50f7
Liam Bowen <LiamBowen@gmail.com>
2016-05-22 15:17:34 -0400

    disable mysterious route; rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,34 @@
 RailsAssetsApp::Application.routes.draw do
-  get '/packages' => 'main#packages'
-  get '/packages/:name' => 'main#package',
-    constraints: { name: /[^\/]+/ }
+  # get '/packages' => 'main#packages'
+  # get '/packages/:name' => 'main#package',
+  #   constraints: { name: /[^\/]+/ }
 
   resources :components, only: [:index, :new, :create] do
     collection do
       get '/:name/rebuild' => 'components#rebuild',
-        constraints: { name: /[^\/]+/ }
+          constraints: { name: /[^\/]+/ }
 
       get '/:name/:version' => 'components#assets',
-        constraints: { version: /[^\/]+/, name: /[^\/]+/ }
+          constraints: { version: /[^\/]+/, name: /[^\/]+/ }
     end
   end
 
-  get "/api/v1/dependencies", to: "main#dependencies"
+  get '/api/v1/dependencies', to: 'main#dependencies'
   resources :donations, only: :create
 
   require 'sidekiq/web'
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [
-      "admin", ENV['SIDEKIQ_PASSWORD'] || "password"
+      'admin', ENV['SIDEKIQ_PASSWORD'] || 'password'
     ]
   end
   mount Sidekiq::Web => '/sidekiq'
 
   get '/status', to: 'main#status'
 
-  root to: "main#home"
+  root to: 'main#home'
 
-  resources :ng_templates, only: :show if !Rails.configuration.x.inline_ng_templates
+  resources :ng_templates, only: :show unless Rails.configuration.x.inline_ng_templates
 
   get '/home', to: redirect('/')
 end
