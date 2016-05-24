07ae9c4767a90418c532676fb395d026b2bc4ec3
Liam Bowen <LiamBowen@gmail.com>
2016-05-23 20:13:29 -0400

    add wopr.rails-assets.org to both

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,5 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 server(
-  'staging.rails-assets.org',
+  'wopr.rails-assets.org',
   user: fetch(:deploy_user),
   roles: %w(app db web worker)
 )
