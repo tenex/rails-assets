491d1fa1d9274bac2b7d90bdd37f9ae5bbaa0515
Liam Bowen <LiamBowen@gmail.com>
2016-05-25 18:37:41 -0400

    remove controller methods

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -69,24 +69,6 @@ class MainController < ApplicationController
     end
   end
 
-  def packages
-    render file: Rails.root.join('public', 'packages.json'),
-           layout: false
-  end
-
-  def package
-    render json: {
-      type: 'alias',
-      url: indexed_packages[params[:name]]['url']
-    }
-  end
-
-  def indexed_packages
-    @indexed_packages ||= JSON.parse(
-      File.read(Rails.root.join('public', 'packages.json'))
-    ).index_by { |p| p['name'] }
-  end
-
   private
 
   def redirect_to_https
