1e8e0bce21b84a850e26bf45dbc96776b3e099de
Liam <LiamBowen@gmail.com>
2022-06-29 22:34:48 -0400

    Fix more deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Capybara.default_driver = :selenium_chrome_headless
 # need to include Capybara::Angular::DSL?
 
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
