2659e8b43d815013df6414b274f47098fefcc79b
Liam Bowen <liambowen@gmail.com>
2022-06-30 00:22:51 +0000

    Add a stacktrace if present from Bower

--- app/models/build/bower_error.rb
+++ app/models/build/bower_error.rb
@@ -2,16 +2,17 @@ module Build
   # Encapsulate Bower errors
   # parsed from Bower JSON output
   class BowerError < BuildError
-    attr_reader :path, :command, :details, :code
+    attr_reader :path, :command, :details, :code, :stacktrace
 
     ENOTFOUND = 'ENOTFOUND'.freeze
     EINVALID = 'EINVALID'.freeze
 
-    def initialize(message, details, path = nil, command = nil, code = nil)
+    def initialize(message, details, path = nil, command = nil, code = nil, stacktrace = nil)
       @details = details
       @path = path
       @command = command
       @code = code
+      @stacktrace = stacktrace
       super(message)
     end
 
@@ -26,8 +27,12 @@ module Build
     def self.from_shell_error(e)
       parsed_json = JSON.parse(e.message)
       error = parsed_json.find { |h| h['level'] == 'error' }
-      BowerError.new(error['message'], error['details'],
-                     e.path, e.command, error['code'])
+      BowerError.new(error['message'],
+                     error['details'],
+                     e.path,
+                     e.command,
+                     error['code'],
+                     error['stacktrace'])
     end
   end
 end
