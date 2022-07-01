1a887ad3bb18aa8f67aafc97f38ba7c169c1ad81
Liam <LiamBowen@gmail.com>
2022-07-01 18:21:16 -0400

    Use stupid workaround for stripe input

--- spec/features/donation_spec.rb
+++ spec/features/donation_spec.rb
@@ -11,8 +11,22 @@ feature 'Stripe integreation' do
 
     Capybara.within_frame 'stripe_checkout_app' do
       fill_in 'Email', with: 'rspec@example.com'
-      fill_in 'Card number', with: '4242 4242 4242 4242'
-      fill_in 'MM / YY', with: 1.year.from_now.strftime('%m/%y')
+      # the next bit *would* be:
+      # fill_in 'Card number', with: '4242 4242 4242 4242'
+      # except that the result is that card_number becomes
+      # 4242 (actually a random subset of above)
+      card_number_field = page.find_by_id('card_number')
+      '4242424242424242'.chars.each do |digit|
+        card_number_field.send_keys digit
+        sleep 0.1
+      end
+
+      # fill_in 'MM / YY', with: 1.year.from_now.strftime('%m/%y')
+      cc_exp_field = page.find_by_id 'cc-exp'
+      1.year.from_now.strftime('%m/%y').chars.each do |char|
+        cc_exp_field.send_keys char
+        sleep 0.1
+      end
       fill_in 'CVC', with: '322'
       click_button 'Pay $5.45'
     end
