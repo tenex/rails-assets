a2e5ff70d8e491971a5e8f9eabf291e71a18c2dc
Liam Bowen <liambowen@gmail.com>
2022-07-15 09:16:52 +0000

    Bower reports name in this case

--- spec/models/build/converter_spec.rb
+++ spec/models/build/converter_spec.rb
@@ -106,13 +106,13 @@ module Build
         end
       end
 
-      #breaks in bower 1.7.9
+      # Case changed in bower 1.7.9
       it 'names the asset paths according to the bower_name that we store in Component' do
         Dir.mktmpdir do |tmpdir|
           gem_path = Converter.install!('adminlte', '2.3.3') do |dependencies|
             Converter.convert!(dependencies.first) do |dir, paths, mains|
               Dir["#{dir}/app/assets/javascripts/*.js"].should eq [
-                "#{dir}/app/assets/javascripts/adminlte.js"
+                "#{dir}/app/assets/javascripts/AdminLTE.js"
               ]
             end
           end
